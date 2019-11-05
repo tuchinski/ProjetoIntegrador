26254c936057ad51f3c7f37e5b5c2f7aefb4b198
tuchinski <tuchinski@alunos.utfpr.edu.br>
2019-11-05 15:00:01 -0300

    criando teste para o arquivo

---
+++ projeto/src/test/java/com/projetoPI/model/DBFileTest.java
@@ -0,0 +1,14 @@
+package com.projetoPI.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DBFileTest {
+
+	@Test
+	public void subirArquivoComNomeNull() {
+		fail("Not yet implemented");
+	}
+
+}
