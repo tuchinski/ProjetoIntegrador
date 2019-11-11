c40d93a94948835ef7f18cfbb96e30646ed1cb52
tuchinski <tuchinski@alunos.utfpr.edu.br>
2019-11-11 20:40:10 -0300

    criando a view do Departamento

---
+++ projeto/src/main/java/com/projetoPI/primefaces/CriaDepartamentoView.java
@@ -0,0 +1,38 @@
+package com.projetoPI.primefaces;
+
+import javax.annotation.ManagedBean;
+import javax.faces.view.ViewScoped;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.projetoPI.service.DepartamentoStorageService;
+
+@ManagedBean
+@ViewScoped
+public class CriaDepartamentoView {
+
+	private String text;
+	
+	@Autowired
+	private DepartamentoStorageService departamentoStorageService;
+	
+	public String getText() {
+		return text;
+	}
+	
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	public void salvaDepartamento() {
+		System.out.println(this.text);
+		if(this.text.isEmpty()) {
+			System.out.println("String vazia");
+		}else {
+			departamentoStorageService.storeDepartamento(text);
+			FacesUtil.addMsgInfo("Categoria Salvo com Sucesso");
+		}
+		this.setText("");
+	}
+	
+}
