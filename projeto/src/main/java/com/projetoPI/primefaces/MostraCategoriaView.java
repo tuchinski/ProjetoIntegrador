80d36e47a063349b8b404ec2f65ac37cc5438ce4
ilzisilva <ilzimarasilva@alunos.utfpr.edu.br>
2019-11-15 17:57:04 -0200

    Crud Categoria front pronto

--- projeto/src/main/java/com/projetoPI/primefaces/MostraCategoriaView.java
+++ projeto/src/main/java/com/projetoPI/primefaces/MostraCategoriaView.java
@@ -2,14 +2,15 @@ package com.projetoPI.primefaces;
 
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
+import org.primefaces.event.RowEditEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 
 import com.projetoPI.model.Categoria;
-
-
 import com.projetoPI.service.CategoriaStorageService;
 
 @Named
@@ -36,5 +37,10 @@ public class MostraCategoriaView {
 
 	public void setSetores(List<Categoria> categorias) {
 		this.categorias = categorias;
+	}
+
+	public void editarLinhaCat(RowEditEvent event) {
+		FacesMessage msg = new FacesMessage("Editar Categoria");
+		FacesContext.getCurrentInstance().addMessage(null, msg);
 	}
 }
