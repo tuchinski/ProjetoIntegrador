29ba5d27d5bb6b6d2582db6b7ccadb690559cc87
tuchinski <tuchinski@alunos.utfpr.edu.br>
2019-11-25 00:12:01 -0300

    iniciando a tela de buscar compêndio

--- projeto/src/main/java/com/projetoPI/service/CompendioStorageService.java
+++ projeto/src/main/java/com/projetoPI/service/CompendioStorageService.java
@@ -15,39 +15,28 @@ public class CompendioStorageService {
 
 	@Autowired
 	private CompendioRepository compendioRepository;
-	
-	public Compendio getCompendio(long compendioId) {
-		return compendioRepository.findById(compendioId)
-				.orElseThrow(() -> new MyFileNotFoundException("Compendio não encontrado com o ID " + compendioId));
+
+	public Compendio storeCompendio(String nome, List<DBFile> arquivos) {
+		Compendio c = new Compendio(nome, arquivos);
+
+		return compendioRepository.save(c);
+	}
+
+	public Compendio getCompendio(Long id) {
+		return this.compendioRepository.findById(id)
+				.orElseThrow(() -> new MyFileNotFoundException("Nao achou compendio com o id " + id));
 	}
 	
 	public List<Compendio> getAllCompendio(){
 		return this.compendioRepository.findAll();
 	}
-	
-	public Compendio storeCompendio(String nome, List<DBFile> arquivos) {
-		Compendio c = new Compendio(nome, arquivos);
-		
-		return compendioRepository.save(c);
-	}
-	
+
 	public void editaCompendio(Compendio c) {
 		compendioRepository.save(c);
 	}
-	
+
 	public void removeCompendio(Compendio c) {
 		compendioRepository.delete(c);
 	}
-	
+
 }
-
-
-
-
-
-
-
-
-
-
-
