116a97c4fad59db481f2f274244aab984b17fe88
tuchinski <tuchinski@alunos.utfpr.edu.br>
2019-11-26 17:08:45 -0300

    listando os arquivos do compendio

--- projeto/src/main/java/com/projetoPI/primefaces/compendio/BuscaCompendioView.java
+++ projeto/src/main/java/com/projetoPI/primefaces/compendio/BuscaCompendioView.java
@@ -4,15 +4,11 @@ import java.util.List;
 
 import javax.inject.Named;
 
-import org.primefaces.model.CheckboxTreeNode;
-import org.primefaces.model.DefaultTreeNode;
-import org.primefaces.model.TreeNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 
 import com.projetoPI.model.Compendio;
 import com.projetoPI.model.DBFile;
-import com.projetoPI.model.TreeDocument;
 import com.projetoPI.service.CompendioStorageService;
 import com.projetoPI.service.DBFileStorageService;
 
@@ -20,54 +16,44 @@ import com.projetoPI.service.DBFileStorageService;
 @Scope("view")
 public class BuscaCompendioView {
 
-	private TreeNode raiz;
 
 	private List<Compendio> compendios;
 	
-	private TreeDocument selectedDocument;
+	private Compendio selectedCompendio;
+	
+	private List<DBFile> documentosCompendio;
 
 	@Autowired
 	private CompendioStorageService compendioStorageService;
 
 	@Autowired
 	private DBFileStorageService dbFileStorageService;
+	
+	public void testa(Compendio c) {
+		System.out.println("entrou Testa");
+		System.out.println(c.getNome());
+		this.selectedCompendio = c;
 
+	}
+	
+
+	public List<DBFile> getDocumentosCompendio() {
+		System.out.println("getDOcsCompendio");
+//		return dbFileStorageService.findByCompendio(this.selectedCompendio);
+		if(this.selectedCompendio == null) {
+			return null;
+		}
+		return this.selectedCompendio.getListaArquivos();
+	}
+	
 	public List<Compendio> getCompendios() {
 		return compendioStorageService.getAllCompendio();
 	}
+
+	public void setCompendios(List<Compendio> compendios) {
+		this.compendios = compendios;
+	}
 	
-	public void testa() {
-		if(this.selectedDocument != null) {
-		  System.out.println("testa " + selectedDocument.getNome());
-		}
-	}
-
-	public TreeNode getRaiz() {
-		TreeNode root = new CheckboxTreeNode(new TreeDocument("Compendios", "-", "Raiz"), null);
-		List<Compendio> comps = compendioStorageService.getAllCompendio();
-		List<DBFile> arqs = dbFileStorageService.getAllFile();
-
-		for (Compendio compendio : comps) {
-			TreeNode filho = new CheckboxTreeNode("Compêndio",new TreeDocument(compendio.getNome(), " - ", "Compêndio"), root);
-			for (DBFile dbFile : arqs) {
-				if (dbFile.getCompendio().getId() == compendio.getId()) {
-					TreeNode neto = new CheckboxTreeNode("documento", new TreeDocument(dbFile.getFile_name(), " - ", dbFile.getFile_type(), dbFile), filho);
-				}
-			}
-		}
-		return root;
-	}
-
-	public void setRaiz(TreeNode raiz) {
-		this.raiz = raiz;
-	}
-
-	public TreeDocument getSelectedDocument() {
-		return selectedDocument;
-	}
-
-	public void setSelectedDocument(TreeDocument selectedDocument) {
-		this.selectedDocument = selectedDocument;
-	}
-
+	
+	
 }
