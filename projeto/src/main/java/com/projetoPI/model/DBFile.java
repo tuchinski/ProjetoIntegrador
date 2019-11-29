f09fc6fd543d1b07ec4ee38074f367564552e99a
tuchinski <tuchinski@alunos.utfpr.edu.br>
2019-11-29 20:22:33 -0300

    criando e listando compendio

--- projeto/src/main/java/com/projetoPI/model/DBFile.java
+++ projeto/src/main/java/com/projetoPI/model/DBFile.java
@@ -58,7 +58,7 @@ public class DBFile {
 	@NotNull
 	private Categoria fileCategoria;
 	
-	@ManyToOne(fetch = FetchType.EAGER)
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "compendio_id")
 	private Compendio compendio;
 
--- projeto/src/main/java/com/projetoPI/primefaces/compendio/BuscaCompendioView.java
+++ projeto/src/main/java/com/projetoPI/primefaces/compendio/BuscaCompendioView.java
@@ -1,9 +1,14 @@
 package com.projetoPI.primefaces.compendio;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.List;
 
 import javax.inject.Named;
 
+import org.primefaces.event.SelectEvent;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 
@@ -17,24 +22,51 @@ import com.projetoPI.service.DBFileStorageService;
 public class BuscaCompendioView {
 
 
+	@SuppressWarnings("unused")
 	private List<Compendio> compendios;
 	
 	private Compendio selectedCompendio;
 	
+	@SuppressWarnings("unused")
 	private List<DBFile> documentosCompendio;
+	
+	private DBFile arquivoSelecionado;
+	
+	private StreamedContent streamedContent;
 
 	@Autowired
 	private CompendioStorageService compendioStorageService;
 
+	@SuppressWarnings("unused")
 	@Autowired
 	private DBFileStorageService dbFileStorageService;
 	
-	public void testa(Compendio c) {
+	public void mostraDocsCompendio(Compendio c) {
 		System.out.println("entrou Testa");
 		System.out.println(c.getNome());
 		this.selectedCompendio = c;
 
 	}
+	
+	public void onRowSelect(SelectEvent event) {		
+		System.out.println("BUSCA COMPENDIO");
+		this.arquivoSelecionado = ((DBFile) event.getObject());	
+		System.out.println(arquivoSelecionado.getFile_name());
+		
+		createStream(arquivoSelecionado);
+
+	}
+	
+	
+	private StreamedContent createStream(DBFile arquivoSelecionado) {
+		InputStream arrayDadosArquivo = new ByteArrayInputStream(arquivoSelecionado.getData());
+		this.streamedContent = new DefaultStreamedContent(arrayDadosArquivo, arquivoSelecionado.getFile_type(), arquivoSelecionado.getFile_name());
+		return this.streamedContent;
+	}
+	
+//	public void aa(DBFile arquivo) {
+//		System.out.println("!!aa!!" + arquivo);
+//	}
 	
 
 	public List<DBFile> getDocumentosCompendio() {
@@ -53,7 +85,18 @@ public class BuscaCompendioView {
 	public void setCompendios(List<Compendio> compendios) {
 		this.compendios = compendios;
 	}
+	public String generateRandomIdForNotCaching() {
+		return java.util.UUID.randomUUID().toString();
+	}
 	
+	public StreamedContent getStreamedContent() {
+		return streamedContent;
+	}
+
+	public void setStreamedContent(StreamedContent streamedContent) {
+		this.streamedContent = streamedContent;
+	}
+
 	
 	
 }
