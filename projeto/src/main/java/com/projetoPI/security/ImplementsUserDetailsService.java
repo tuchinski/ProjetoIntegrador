a9b425b6267d52065ec108c8cc3e02f127dd494e
tuchinski <tuchinski@alunos.utfpr.edu.br>
2019-12-01 16:47:25 -0300

    logando na aplicação com funcionário

--- projeto/src/main/java/com/projetoPI/security/ImplementsUserDetailsService.java
+++ projeto/src/main/java/com/projetoPI/security/ImplementsUserDetailsService.java
@@ -1,29 +1,31 @@
 package com.projetoPI.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Repository;
 
-import com.projetoPI.model.Usuario;
-import com.projetoPI.repository.UsuarioRepository;
+import com.projetoPI.model.Funcionario;
+import com.projetoPI.service.FuncionarioStorageService;
 
 @Repository
 public class ImplementsUserDetailsService implements UserDetailsService{
 	
 	@Autowired
-	private UsuarioRepository ur;
+	private FuncionarioStorageService funcionarioStorageService;
 
 	@Override
 	public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
-		Usuario usuario = ur.findByLogin(login);
+		Funcionario funcionario = funcionarioStorageService.findByLogin(login);
 		
 		
-		if(usuario == null) {
+		if(funcionario == null) {
 			throw new UsernameNotFoundException("Usuario não encontrado");			
 		}		
-		return usuario;
+//		return usuario;
+		return new User(funcionario.getUsername(), funcionario.getPassword(), true, true, true, true, funcionario.getAuthorities());
 	}
 
 }
--- projeto/src/main/java/com/projetoPI/security/WebSecurityConfig.java
+++ projeto/src/main/java/com/projetoPI/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -18,26 +19,40 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 		  protected void configure(HttpSecurity http) throws Exception {
 			
 		    // require all requests to be authenticated except for the resources
-		    http.authorizeRequests().antMatchers("/javax.faces.resource/**")
-		        .permitAll().anyRequest().authenticated();
+//		    http.authorizeRequests().antMatchers("/javax.faces.resource/**")
+//		        .permitAll().anyRequest().authenticated();
+		    
+		    http.authorizeRequests()
+		    .mvcMatchers("/cadastroFunc.xhtml").hasRole("ADMIN")
+		    .mvcMatchers("/cadastraCompendio.xhtml").hasRole("ADMIN");
+		    
 		    
 		    // login
 		    http.formLogin().loginPage("/login.xhtml").permitAll()
-		        .failureUrl("/login.xhtml?error=true").defaultSuccessUrl("/home.xhtml");
-		    
-		    // logout
-		    http.logout().logoutSuccessUrl("/login.xhtml");
+		        .failureUrl("/login.xhtml?error=true").defaultSuccessUrl("/buscarCompendio.xhtml").and().
+		        logout().logoutSuccessUrl("/login.xhtml");
 		    
 		    // not needed as JSF 2.2 is implicitly protected against CSRF
 		    http.csrf().disable();
+		    
 		  }
+		
+		@Override
+		protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+			auth.userDetailsService(userDetailsService)
+			.passwordEncoder(new BCryptPasswordEncoder());
+		}
 
-		  @Autowired
-		  public void configureGlobal(AuthenticationManagerBuilder auth)
-		      throws Exception {
-		    auth.inMemoryAuthentication().withUser("john.doe")
-		        .password("{noop}1234").roles("USER").and()
-		        .withUser("jane.doe").password("{noop}5678").roles("ADMIN");
-		  }
+
+//		  @Autowired
+//		  public void configureGlobal(AuthenticationManagerBuilder auth)
+//		      throws Exception {
+//		    auth.inMemoryAuthentication().withUser("john.doe")
+//		        .password("{noop}1234").roles("USER").and()
+//		        .withUser("jane.doe").password("{noop}5678").roles("ADMIN");
+//			  
+//			auth.userDetailsService(userDetailsService)
+//			.passwordEncoder(new BCryptPasswordEncoder());
+//		  }
 
 }
