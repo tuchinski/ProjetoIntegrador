20bbcaba898889a2c7c6d61a1684df1ced624818
tuchinski <tuchinski@alunos.utfpr.edu.br>
2019-12-06 21:16:38 -0300

    mudando redirect para login

--- projeto/src/main/resources/data.sql
+++ projeto/src/main/resources/data.sql
@@ -5,7 +5,9 @@ insert into role(nome_role) values
 	('ROLE_FUNCIONARIO'),
 	('ROLE_DIRETOR');
 
-CREATE TABLE TESTE(
-	a INTEGER,
-	b FLOAT
-);
+INSERT INTO funcionario VALUES 
+('000.000.000-00,'admin@admin.com','123','ADMIN','ADMIN','ADMIN','$2a$10$S.HAItSaVWpa3uHHvu4l3.ZpMVCBrqdTrQKGYLkgZUmiHMi.TZ.hG','11asd','M','ADMIN');
+
+INSERT INTO funcionarios_roles VALUES 
+('000.000.000-00','ROLE_ADMIN');
+
