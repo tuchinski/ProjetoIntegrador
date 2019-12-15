bfe1c38593126c9e8c30e24a5210c3c75b770174
tuchinski <tuchinski@alunos.utfpr.edu.br>
2019-12-14 23:02:30 -0300

    arrumando script para inserir o admin

--- projeto/src/main/resources/data.sql
+++ projeto/src/main/resources/data.sql
@@ -1,7 +1,11 @@
-
-insert into role(nome_role) values
+insert into projPI.role(nome_role) values
 	('ROLE_ADMIN'),
-	('ROLE_USER'),
 	('ROLE_GERENTE'),
 	('ROLE_FUNCIONARIO'),
-	('ROLE_DIRETOR');
+	('ROLE_DIRETOR'),
+	('ROLE_CHEFEDEPTO');
+
+INSERT INTO `projPI`.`funcionario` (`cpf`, `e_mail`, `rg`, `cargo`, `endereco`, `nome_funcionario`, `senha`, `sexo`, `telefone`)
+VALUES ('000.000.000-00', 'admin@admin.com', '0', 'ROLE_ADMIN', '0', 'ADMIN', '$2a$10$S.HAItSaVWpa3uHHvu4l3.ZpMVCBrqdTrQKGYLkgZUmiHMi.TZ.hG', 'F', '(44)0000-0000');
+
+INSERT INTO `projPI`.`funcionarios_roles` (`funcionario_id`, `role_id`) VALUES ('000.000.000-00', 'ROLE_ADMIN');
